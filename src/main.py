d9f2c7c8ba0684956c8157a24fd6f12a3ee2d8c2
one_victus <marvinjesus28@gmail.com>
2025-02-17 09:18:49 -0400

    Ini project's structure

---
+++ src/main.py
@@ -0,0 +1,4 @@
+#from string_pg import str_prueba
+import src.string_pg as strpg
+
+strpg.str_prueba()
---
+++ src/string_pg.py
@@ -0,0 +1,8 @@
+def str_prueba():
+    prueba = 'hola'
+    prueba_2 = '  victus  '
+
+    print(prueba[-1])
+    print(prueba * 3)
+    print(len(prueba) > 2)
+    print(prueba_2.strip()) # equivalente a trim
